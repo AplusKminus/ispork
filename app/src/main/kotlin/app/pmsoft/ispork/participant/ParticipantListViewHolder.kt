7ed5967dfa47a0e809c05bf136dfb7dabf2d33b2
0plusKminus <0pluskminus@gmail.com>
2020-01-05 19:24:17 +0100

    #16 Add icons to Participants in participant only list views
    Removed inner ViewHolder class from ParticipantViewAdapter.

--- app/src/main/kotlin/app/pmsoft/ispork/participant/ParticipantListViewHolder.kt
+++ app/src/main/kotlin/app/pmsoft/ispork/participant/ParticipantListViewHolder.kt
@@ -2,11 +2,13 @@ package app.pmsoft.ispork.participant
 
 import android.view.View
 import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.TextView
 import app.pmsoft.ispork.AbstractViewHolder
 import app.pmsoft.ispork.R
 import app.pmsoft.ispork.SelectionHandler
 import app.pmsoft.ispork.data.FullParticipant
+import app.pmsoft.ispork.data.Participant
 import app.pmsoft.ispork.util.CurrencyHandler
 
 class ParticipantListViewHolder(
@@ -19,6 +21,7 @@ class ParticipantListViewHolder(
   private val layout: FrameLayout = view.findViewById(R.id.participant_list_item_layout)
   private val amountLabel: TextView = view.findViewById(R.id.participant_list_item_amount_label)
   private val nameLabel: TextView = view.findViewById(R.id.participant_list_item_name_label)
+  private val participantTypeIcon: ImageView = view.findViewById(R.id.participant_list_item_participant_type_icon)
 
   override val backgroundView: View
     get() = layout
@@ -28,6 +31,16 @@ class ParticipantListViewHolder(
     if (e.type.internal) {
       amountLabel.visibility = View.VISIBLE
       amountLabel.text = CurrencyHandler.format((e.balance ?: 0L) + (e.startingBalance ?: 0L))
+    } else {
+      amountLabel.visibility = View.GONE
     }
+    participantTypeIcon.setImageDrawable(itemView.context.resources.getDrawable(
+      when (e.type){
+        Participant.Type.ACCOUNT -> R.drawable.ic_account_white_24dp
+        Participant.Type.PERSON -> R.drawable.ic_person_white_24dp
+        Participant.Type.PAYEE -> R.drawable.ic_payee_white_24dp
+      },
+      itemView.context.theme
+    ))
   }
 }
